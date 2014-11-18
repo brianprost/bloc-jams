0ac6eeb114e97c151e30bc614ab5d8b931e12387
Brian Prost <bprost94@gmail.com>
2014-11-18 23:41:03 +0000

    made our music player play music!

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -291,11 +291,11 @@ var albumPicasso = {
     year: '1881',
     albumArtUrl: '/images/album-placeholder.png',
     songs: [
-        {name: 'Blue', length: '4:26'},
-        {name: 'Green', length: '3:14'},
-        {name: 'Red', length: '5:01'},
-        {name: 'Pink', length: '3:21'},
-        {name: 'Magenta', length: '2:15'},
+        {name: 'Blue', length: '4:26', audioUrl: '/music/placeholders/blue'},
+        {name: 'Green', length: '3:14', audioUrl: '/music/placeholders/green'},
+        {name: 'Red', length: '5:01', audioUrl: '/music/placeholders/red'},
+        {name: 'Pink', length: '3:21', audioUrl: '/music/placeholders/pink'},
+        {name: 'Magenta', length: '2:15', audioUrl: '/music/placeholders/magenta'},
     ]
 };
 
@@ -344,12 +344,16 @@ blocJams.controller('Landing.controller', ['$scope', function($scope) {
     ];
 }]);
 
-blocJams.controller('Collection.controller', ['$scope', function($scope) {
+blocJams.controller('Collection.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
     $scope.albums = [];
     for (var i = 0; i < 33; i++) {
         $scope.albums.push(angular.copy(albumPicasso));
     }
-}])
+    
+    $scope.playAlbum = function(album) {
+        SongPlayer.setSong(album, album.songs[0]); // Targets first song in the array
+    };
+}]);
 
 blocJams.controller('Album.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
     $scope.album = angular.copy(albumPicasso);
@@ -375,8 +379,8 @@ blocJams.controller('Album.controller', ['$scope', 'SongPlayer', function($scope
     };
     
     $scope.playSong = function(song) {
+        // something isn't supposed to be here....
         SongPlayer.setSong($scope.album, song);
-        SongPlayer.play();
     };
     
     $scope.pauseSong = function(song) {
@@ -389,6 +393,8 @@ blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($s
 }]);
 
 blocJams.service('SongPlayer', function () {
+    var currentSoundFile = null;
+    
     var trackIndex = function(album, song) {
         return album.songs.indexOf(song);
     };
@@ -400,9 +406,11 @@ blocJams.service('SongPlayer', function () {
         
         play: function() {
             this.playing = true;
+            currentSoundFile.play();
         },
         pause: function() {
             this.playing = false;
+            currentSoundFile.pause();
         },
         next: function() {
             var currentTrackIndex = trackIndex(this.currentAlbum, this.currentSong);
@@ -410,7 +418,9 @@ blocJams.service('SongPlayer', function () {
             if (currentTrackIndex >= this.currentAlbum.songs.length) {
                 currentTrackIndex = 0;
             }
-            this.currentSong = this.currentAlbum.songs[currentTrackIndex];
+            var song = this.currentAlbum.songs[currentTrackIndex];
+            this.setSong(this.currentAlbum, song);
+            // this.currentSong = this.currentAlbum.songs[currentTrackIndex];
         },
         previous: function() {
             var currentTrackIndex = trackIndex(this.currentAlbum, this.currentSong);
@@ -418,11 +428,22 @@ blocJams.service('SongPlayer', function () {
             if (currentTrackIndex < 0) {
                 currentTrackIndex = this.currentAlbum.songs.length - 1;
             }
-            this.currentSong = this.currentAlbum.songs[currentTrackIndex];
+            // this.currentSong = this.currentAlbum.songs[currentTrackIndex];
+            var song = this.currentAlbum.songs[currentTrackIndex];
+            this.setSong(this.currentAlbum, song);
         },
         setSong: function(album, song) {
+            if (currentSoundFile) {
+                currentSoundFile.stop();
+            }
             this.currentAlbum = album;
             this.currentSong = song;
+            currentSoundFile = new buzz.sound(song.audioUrl, {
+                formats: ["mp3"],
+                preload: true
+            });
+            
+            this.play();
         }
     };
 });
